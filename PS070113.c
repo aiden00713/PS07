5c99bca04ad76ffcdbee4c8bb65ddf3e830ff1dc
aiden00713 <44128720+aiden00713@users.noreply.github.com>
2019-10-15 21:12:30 +0800

    Update PS070113.c

--- PS070113.c
+++ PS070113.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-	int n,i,j;
+	int n,i,j=0;
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
-	if(n%i==0)
-	j++;
-	printf("%d",j-1);
+	{
+		if(n%i==0)
+		j++;
+	}
+
+	printf("%d",j);
 	
 	return 0;
 }
